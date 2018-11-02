1c35df880bd09cef3d535cc83f02e28ac945ee27
Alexander Potts <alexander.potts@gmail.com>
2018-11-01 18:01:27 -0600

    Got message grouping mechanism to work properly

--- src/messageWindow/msgWindow.js
+++ src/messageWindow/msgWindow.js
@@ -3,10 +3,11 @@ import React from 'react';
 import {Message} from './singleMessage';
 
 export function MsgWindow(props) {
-  const {messages, lastUsername} = props;
+  const {messages} = props;
   return (
     <div id="message-window">
-      {messages.map((message) => {
+      {messages.map((message, index) => {
+        const lastUsername = (index > 0) ? messages[index-1].username: '';
         return <Message message={message} lastUsername={lastUsername}/>
         }
       )}
